c3b80b428632ff9b7a3b998fd7667923ace8fc69
ikngtty <ikngtty@gmail.com>
2022-07-14 19:31:04 +0900

    get html

--- pkg/qns/qns.go
+++ pkg/qns/qns.go
@@ -2,8 +2,37 @@ package qns
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func Load() {
-	fmt.Println("hello")
+	req, err := http.NewRequest("GET", "https://qiita.com/notifications", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	req.AddCookie(&http.Cookie{
+		Name:  "user_session_key",
+		Value: os.Getenv("qiita_user_session_key")})
+	req.AddCookie(&http.Cookie{
+		Name:  "secure_token",
+		Value: os.Getenv("qiita_secure_token")})
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(doc.Html())
 }
