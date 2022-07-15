06ff94a641885d51f658dcb45c550e1e3a6d242c
ikngtty <ikngtty@gmail.com>
2022-07-15 16:37:53 +0900

    fetch notifications

--- pkg/qns/qns.go
+++ pkg/qns/qns.go
@@ -8,6 +8,11 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type notification struct {
+	Kind string
+	Text string
+}
+
 func Load() {
 	req, err := http.NewRequest("GET", "https://qiita.com/notifications", nil)
 	if err != nil {
@@ -34,5 +39,37 @@ func Load() {
 		os.Exit(1)
 	}
 
-	fmt.Println(doc.Html())
+	notifications := make([]notification, 0)
+	doc.Find(".notification_actionWrapper").Each(func(index int, wrapper *goquery.Selection) {
+		contents := selectionToSlice(wrapper.Contents())
+		var kind string
+		if len(contents) == 7 && contents[4].HasClass("bold") && contents[4].Text() == "LGTM" {
+			kind = "LGTM"
+		} else if len(contents) == 7 && contents[4].HasClass("bold") && contents[4].Text() == "ストック" {
+			kind = "ストック"
+		} else if len(contents) == 7 && contents[4].HasClass("bold") && contents[4].Text() == "コメント" {
+			kind = "コメント"
+		} else if len(contents) == 7 && contents[4].HasClass("bold") && contents[4].Text() == "編集リクエスト" {
+			kind = "編集リクエスト"
+		} else if len(contents) == 7 && contents[4].HasClass("bold") && contents[4].Text() == "リンク" {
+			kind = "リンク"
+		} else if len(contents) == 7 && contents[4].HasClass("bold") && contents[4].Text() == "編集" {
+			kind = "編集"
+		} else {
+			kind = "その他"
+		}
+		notifications = append(notifications, notification{kind, wrapper.Text()})
+	})
+
+	for _, notifi := range notifications {
+		fmt.Printf("kind:%s text:%s\n", notifi.Kind, notifi.Text)
+	}
+}
+
+func selectionToSlice(sel *goquery.Selection) []*goquery.Selection {
+	sels := make([]*goquery.Selection, sel.Length())
+	sel.Each(func(index int, sel *goquery.Selection) {
+		sels[index] = sel
+	})
+	return sels
 }
